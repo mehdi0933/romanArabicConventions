430a8de91bfc427b11e401c25aaebf5983db3caf
mehdi0933 <73241579+mehdi0933@users.noreply.github.com>
2025-02-26 00:03:56 +0100

    Add files via upload
    correction

--- Main.java
+++ Main.java
@@ -1,5 +1,37 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        LinkedHashMap<Integer, String> toTranslateArabeRoman = new LinkedHashMap<>();
+        toTranslateArabeRoman.put(1000,"M");
+        toTranslateArabeRoman.put(900, "CM");
+        toTranslateArabeRoman.put(100, "C");
+        toTranslateArabeRoman.put(90,  "XC");
+        toTranslateArabeRoman.put(50,  "D");
+        toTranslateArabeRoman.put(10,  "X");
+        toTranslateArabeRoman.put(9,   "IX");
+        toTranslateArabeRoman.put(5,   "V");
+        toTranslateArabeRoman.put(4,   "IV");
+        toTranslateArabeRoman.put(1,   "I");
+
+        romanArabicConventions(3999, toTranslateArabeRoman);
+
+
     }
+    public static void romanArabicConventions( int nomberArabe,Map <Integer, String> toTranslateArabeRoman ){
+        for (Map.Entry<Integer, String> Entry  :toTranslateArabeRoman.entrySet()){
+            while (nomberArabe >= Entry.getKey() ) {
+                System.out.print(Entry.getValue());
+                nomberArabe = nomberArabe-Entry.getKey();
+
+            }
+        }
+    }
+
+
+
 }
